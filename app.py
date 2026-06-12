0c563bec344e7fe8655f4e67cf6e083f31640727
OSL-biassono <coordinatori@parrocchiabiassono.it>
2026-06-12 16:22:29 +0200

    Delete app.py

--- app.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-pg = st.navigation([st.Page("Home.py"), st.Page("Prima Fascia.py"), st.Page("Seconda Fascia.py"), st.Page("Terza Fascia.py"), st.Page("Quarta Fascia.py")])
-pg.run()
-st.logo("Loghi/Logo Oratorio S.Luigi - Scritte Bianche.png", size="large", link="https://comunitapastoralebms.it/biassono/")
-
